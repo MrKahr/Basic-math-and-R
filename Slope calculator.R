5c6024983ce5d703551c9f211b5c5766ce9b5e77
Mads <70133908+MrKahr@users.noreply.github.com>
2021-05-06 10:08:40 +0200

    Update Slope calculator.R

--- Slope calculator.R
+++ Slope calculator.R
@@ -1,13 +1,11 @@
-#Variables
-x_1 <- 1
-x_2 <- 1.001
-
-y_1 <- sin((10*pi)/x_1)
-y_2 <- sin((10*pi)/x_2)
-
-slope_calc <- function(x_1,x_2,y_1,y_2){
+slope_calc <- function(...){
+  x_1 <- as.integer(readline(prompt="Enter x1: "))
+  x_2 <- as.integer(readline(prompt="Enter x2: "))
+  y_1 <- as.integer(readline(prompt="Enter y1: "))
+  y_2 <- as.integer(readline(prompt="Enter y2: "))
   slope <- (y_2-y_1)/(x_2-x_1)
   return(slope)
 }
-slope_c alc(x_2,x_1,y_2,y_1)
+slope_calc()
+
 
